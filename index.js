0a342aa1791fdbe5d952d6aaf91bb7c3147d7de6
Ara Kevonian <ara.kevonian@gmail.com>
2021-06-08 20:18:12 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Avatar } from './lib/avatar'
+import { MediaStreams, VideoAvatar, VRSPACEUI, World, WorldManager } from './lib/vrspace-ui'
+import { VRSPACE } from './lib/vrspace';
+
+export {
+  Avatar,
+  MediaStreams,
+  VideoAvatar,
+  VRSPACEUI,
+  World,
+  WorldManager,
+  VRSPACE
+}
